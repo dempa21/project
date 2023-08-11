6f04dc1c000889fde0bb7f4f890c74cc9a65c60c
Demian <demparom@gmail.com>
2023-08-11 18:42:00 -0300

    Tercer cambio

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -12,9 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, `${__dirname}/../public/images`);
-  },
+  destination:  "./images",
   filename: function (req, file, cb) {
     cb(null, `${Date.now()}-${file.originalname}`);
   },
